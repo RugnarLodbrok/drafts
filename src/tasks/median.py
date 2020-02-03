bb47ee31a9eb079ada493cc8e1ee6a03014abe48
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-02-03 17:59:13 +0300

    merge linked lists WIP: implement insort

--- src/tasks/median.py
+++ src/tasks/median.py
@@ -4,8 +4,62 @@ from random import randrange
 from py_tools.seq import nth
 
 
+class list2(list):
+    def __init__(self, iterable):
+        super().__init__(iterable)
+        self.upper_i = len(self)
+        self.lower_i = 0
+
+    @property
+    def inner_len(self):
+        return self.upper_i - self.lower_i
+
+    @property
+    def lower(self):
+        return self[self.lower_i]
+
+    @property
+    def upper(self):
+        return self[self.upper_i - 1]
+
+
+class _MedianAlgState:
+    def __init__(self, a, b):
+        self.a = list2(a)
+        self.b = list2(b)
+        self.upper_n = 0
+        self.lower_n = 0
+        self.upper = None
+        self.lower = None
+
+    def solve(self):
+        # 1. ranges of a and be are not intersecting
+        a = self.a
+        b = self.b
+        assert (a[-1] <= b[0]) or (b[-1] <= a[0]), "NIE"
+        if a[0] > b[0]:
+            a, b = b, a
+        lower_n = 0
+        upper_n = 0
+        if len(a) >= len(b):
+            upper_n = len(b)
+            arr = a
+        else:
+            lower_n = len(a)
+            arr = b
+        while arr.inner_len > 1:
+            if upper_n > lower_n:
+                lower_n += arr.inner_len // 2
+                arr.lower_i += arr.inner_len // 2
+            else:
+                upper_n += (arr.inner_len + 1) // 2
+                arr.upper_i -= (arr.inner_len + 1) // 2
+        ret = arr.lower
+        return ret
+
+
 def median(a, b):
-    return 0
+    return _MedianAlgState(a, b).solve()
 
 
 def my_median(a, b):
@@ -28,6 +82,7 @@ def my_median(a, b):
                     y = next(b)
                 except StopIteration:
                     yield x
+                    yield from a
                     return
             else:
                 yield x
@@ -35,18 +90,67 @@ def my_median(a, b):
                     x = next(a)
                 except StopIteration:
                     yield y
+                    yield from b
                     return
 
     len_2 = (len(a) + len(b)) // 2
     return nth(merger(a, b), len_2)
 
 
+def median_orig(A, B):
+    m, n = len(A), len(B)
+    if m > n:
+        A, B, m, n = B, A, n, m
+    if n == 0:
+        raise ValueError
+
+    imin, imax, half_len = 0, m, (m + n + 1) / 2
+    while imin <= imax:
+        i = (imin + imax) / 2
+        j = half_len - i
+        if i < m and B[j - 1] > A[i]:
+            # i is too small, must increase it
+            imin = i + 1
+        elif i > 0 and A[i - 1] > B[j]:
+            # i is too big, must decrease it
+            imax = i - 1
+        else:
+            # i is perfect
+
+            if i == 0:
+                max_of_left = B[j - 1]
+            elif j == 0:
+                max_of_left = A[i - 1]
+            else:
+                max_of_left = max(A[i - 1], B[j - 1])
+
+            if (m + n) % 2 == 1:
+                return max_of_left
+
+            if i == m:
+                min_of_right = B[j]
+            elif j == n:
+                min_of_right = A[i]
+            else:
+                min_of_right = min(A[i], B[j])
+
+            return (max_of_left + min_of_right) / 2.0
+
+
 if __name__ == '__main__':
-    a = list(sorted(randrange(1000) for _ in range(100)))
-    b = list(sorted(randrange(1000) for _ in range(55)))
-    print(a, b)
-    print(median(a, b))
-    print(my_median(a, b))
+    a = list(sorted(randrange(50) for _ in range(100)))
+    b = list(sorted(randrange(70, 120) for _ in range(55)))
+    print(f"non-intersecting:\t{median(a, b)} vs {my_median(a, b)}")
+    print(f"non-intersecting:\t{median(b, a)} vs {my_median(b, a)}")
+
+    a = list(sorted(randrange(70, 120) for _ in range(100)))
+    b = list(sorted(randrange(50) for _ in range(55)))
+    print(f"non-intersecting:\t{median(a, b)} vs {my_median(a, b)}")
+    print(f"non-intersecting:\t{median(b, a)} vs {my_median(b, a)}")
+
+    # a = list(sorted(randrange(1000) for _ in range(100)))
+    # b = list(sorted(randrange(1000) for _ in range(55)))
+    # print(f"general case:\t{median(a, b)} vs {my_median(a, b)}")
 
 if __name__ == '__main__':
     pass
--- src/tasks/merge_k_sorted_lists.py
+++ src/tasks/merge_k_sorted_lists.py
@@ -1,2 +1,114 @@
+from py_tools.seq import isplit
+import bisect
+
+
+def insort(a: list, x, key=None):
+    if not key:
+        key = lambda x: x
+    i = 0
+    j = len(a)
+    while True:
+        mid = (i + j) // 2
+        if key(x) >= key(a[mid]):
+            if i == mid:
+                break
+            i = mid
+        else:
+            j = mid
+    a.insert(i, x)
+    return i
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.len = 0
+
+    def push_tail(self, v):
+        if not isinstance(v, ListNode):
+            v = ListNode(v)
+        else:
+            v.next = None
+        if not self.tail:
+            self.head = v
+            self.tail = v
+        self.tail.next = v
+        self.tail = v
+        self.len += 1
+
+    def push_head(self, v):
+        if not isinstance(v, ListNode):
+            v = ListNode(v)
+        else:
+            v.next = None
+        v.next = self.head
+        self.head = v
+        if not self.tail:
+            self.tail = self.head
+        self.len += 1
+
+    def pop_head(self):
+        if not self.len:
+            return None
+        r = self.head
+        self.head = r.next
+        if not self.head:
+            self.tail = None
+        self.len -= 1
+        return r
+
+    @classmethod
+    def from_string(cls, s, type=None):
+        """
+        :param s: `1->1->2->3->4->4->5->6`
+        """
+        r = LinkedList()
+        for v in isplit(s, '->'):
+            if type is not None:
+                v = type(v)
+            r.push_tail(v)
+        return r
+
+    def __len__(self):
+        return self.len
+
+    def __bool__(self):
+        return self.len
+
+    def __str__(self):
+        return "->".join(str(x) for x in self.head)
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __iter__(self):
+        curr = self
+        while curr:
+            yield curr.val
+            curr = curr.next
+
+
+def merge(lists):
+    r = LinkedList()
+    lists = sorted(lists, key=lambda a: a.head.val)
+    while lists:
+        r.push_tail(lists[0].pop_head())
+        if not lists[0]:
+            lists.pop(0)
+        else:
+            bisect.insort()
+
+
 if __name__ == '__main__':
-    pass
+    # merge([
+    #     LinkedList.from_string("1->4->5"),
+    #     LinkedList.from_string("2->6"),
+    #     LinkedList.from_string("1->3->4"),
+    # ])
+    a = [1, 2, 3, 4, 5, 5, 5, 5, 6, 6, 7]
+    i = insort(a, 5)
+    print(a[:i], a[i], a[i + 1:])
