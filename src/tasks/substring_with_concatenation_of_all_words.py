f34cc2e13fd8025d788ab23f813e7f888c62a199
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-02-05 18:51:22 +0300

    add regex-based solution

--- src/tasks/substring_with_concatenation_of_all_words.py
+++ src/tasks/substring_with_concatenation_of_all_words.py
@@ -24,11 +24,15 @@ Input:
 Output: []
 
 """
+import re
 from collections import defaultdict
 from typing import List
 
 
-def find_substring(s: str, words: List[str]) -> List[int]:
+def find_substring(s: str, words: List[str]) -> List[int]:  # 21 us
+    """
+    fastest so far
+    """
     ret = []
     if not words:
         return ret
@@ -59,12 +63,43 @@ def find_substring(s: str, words: List[str]) -> List[int]:
     return ret
 
 
-def find_substring_non_recur(s: str, words: List[str]) -> List[int]:
+def find_substring_re(s: str, words: List[str]) -> List[int]:  # 33 us
     """
-    When len(words) > python stack size
-    :param s:
-    :param words:
-    :return:
+    non-recursive, based on regex
+    """
+    ret = []
+    if not words:
+        return ret
+    total_w_len = sum(map(len, words))
+    ww = defaultdict(int)
+    for w in words:
+        ww[w] += 1
+
+    def recur(i):  # pass i instead of cut string to avoid copying memory
+        words = {**ww}
+        while True:
+            words_pattern = "|".join(w for w, c in words.items() if c)
+            if not words_pattern:
+                return True
+            match = re.match(fr"^.{{{i}}}({words_pattern})", s)
+            if not match:
+                return False
+            w = match.group(1)
+            words[w] -= 1
+            i += len(w)
+
+    for m in re.finditer("(" + "|".join(ww) + ")", s):
+        i = m.start()
+        if total_w_len + i > len(s):
+            break
+        if recur(i):
+            ret.append(i)
+    return ret
+
+
+def find_substring_non_recur(s: str, words: List[str]) -> List[int]:  # 60us
+    """
+    the slowest but doesnt fail when len(words) > python stack size
     """
     ret = []
     if not words:
@@ -113,12 +148,13 @@ def find_substring_non_recur(s: str, words: List[str]) -> List[int]:
 
 
 def main():
-    print(find_substring_non_recur(s="barfoothebarfoobarman", words=["foo", "bar"]))
-    print(find_substring_non_recur(s="a" * 36, words=["a"] * 8))
-    print(find_substring_non_recur(s="barfoothebarfoobarman", words=[]))
-    print(find_substring_non_recur("wordgoodgoodgoodbestword", ["word", "good", "best", "word"]))
-    print(find_substring_non_recur("a" * 100, ["a"] * 101))
-    print(find_substring_non_recur("a" * 5000, ["a"] * 5001))
+    print(find_substring_re(s="barfoothebarfoobarman", words=["foo", "bar"]))  # this was used for the benchmark
+    print(find_substring_re(s="a" * 36, words=["a"] * 8))
+    print(find_substring_re(s="barfoothebarfoobarman", words=[]))
+    print(find_substring_re("wordgoodgoodgoodbestword", ["word", "good", "best", "word"]))
+    print(find_substring_re("a" * 100, ["a"] * 101))
+    print(find_substring_re("a" * 500, ["a"] * 499))
+    print(find_substring_re("a" * 5000, ["a"] * 5001))
 
 
 if __name__ == '__main__':
