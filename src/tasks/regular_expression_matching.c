37a46846f8119e5493872ae22742d24b8902616d
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-02-06 16:49:02 +0300

    regex match task

--- src/tasks/regular_expression_matching.c
+++ src/tasks/regular_expression_matching.c
@@ -1,16 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   nmatch.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ksticks <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/15 18:12:33 by ksticks           #+#    #+#             */
-/*   Updated: 2019/06/15 18:12:33 by ksticks          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
 
-static unsigned int	ft_strlen(char *str)
+https://leetcode.com/problems/regular-expression-matching/
+
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Note:
+
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like . or *.
+Example 1:
+
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input:
+s = "aa"
+p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+Example 3:
+
+Input:
+s = "ab"
+p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+Example 4:
+
+Input:
+s = "aab"
+p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+Example 5:
+
+Input:
+s = "mississippi"
+p = "mis*is*p*."
+Output: false
+*/
+
+static unsigned int ft_strlen(char *str)
 {
 	int len;
 
@@ -22,7 +60,7 @@ static unsigned int	ft_strlen(char *str)
 	return (len);
 }
 
-static unsigned int	star_count(char *str)
+static unsigned int star_count(char *str)
 {
 	int n;
 
@@ -33,7 +71,7 @@ static unsigned int	star_count(char *str)
 	return (n);
 }
 
-static int  		recur(char *s1, char *s2, unsigned int star_len)
+static int recur(char *s, char *p, unsigned int star_len)
 {
 	unsigned int i;
 	unsigned int n;
@@ -41,31 +79,48 @@ static int  		recur(char *s1, char *s2, unsigned int star_len)
 	n = 0;
 	while (1)
 	{
-		if (*s2 == '*')
+		if (*p && p[1] == '*')  // current char is asketisked
 		{
-			i = 0;
-			while (i++ <= star_len)
-				n += recur(s1 + i - 1, s2 + 1, star_len - i + 1);
-			return (n);
+			i = -1;
+			while (++i <= star_len)  //star_len + 1  tris;  0..star_len
+			{
+				if (i && (s[i - 1] != *p && *p != '.'))
+					break;
+				if (recur(s + i, p + 2, star_len - i))
+					return (1);
+			}
+			return (0); //  asterisk didn't match
 		}
-		if (!*s1)
-		{
-			if (!*s2)
-				n++;
-			return (n);
-		}
-		if (!*s2)
-			return (n);
-		if (*s1++ != *s2++)
-			return (n);
+		if (!*s)
+			return (!*p);
+		if (!*p)
+			return (0);
+		if (*s != *p && *p != '.')
+			return (0);
+		s++;
+		p++;
 	}
 }
 
-int				    nmatch(char *s1, char *s2)
+int nmatch(char *s1, char *s2)
 {
 	return (recur(s1, s2, ft_strlen(s1) - ft_strlen(s2) + star_count(s2)));
 }
 
-bool isMatch(char * s, char * p){
-	return !!(recur(s1, s2, ft_strlen(s1) - ft_strlen(s2) + star_count(s2)));
+bool isMatch(char *s, char *p)
+{
+	//assume there's never more then one asterisk in a row
+	int star_len = ft_strlen(s) - ft_strlen(p) + 2 * star_count(p);
+	//total length of asterisk-matched characters
+	if (star_len < 0)
+		return (0);
+	return recur(s, p, star_len);
+}
+
+int main(int ac, char **av)
+{
+	char *p = av[1];
+	char *s = av[2];
+	printf("`%s` matches `%s`: %d\n", p, s, isMatch(s, p));
+	return 0;
 }
