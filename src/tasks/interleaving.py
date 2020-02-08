d9908c9ebee74822146395e3d46bbe7d4f9cd9c2
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-02-08 21:00:52 +0300

    interleaving done

--- src/tasks/interleaving.py
+++ src/tasks/interleaving.py
@@ -13,6 +13,8 @@ Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
 """
 
+import numpy as np
+
 
 def interleaving_naive(s1, s2, s3):
     len1 = len(s1)
@@ -43,11 +45,38 @@ def interleaving_naive(s1, s2, s3):
     return recur(0, 0, 0)
 
 
+def interleaving_dynamic(s1, s2, s3):
+    if len(s1) + len(s2) != len(s3):
+        return False
+
+    if len(s1) < len(s2):
+        s2, s1 = s1, s2
+
+    current = None
+    for j in range(len(s2) + 1):
+        previous = current
+        current = np.zeros(len(s1) + 1, dtype=np.bool)
+        for i in range(len(s1) + 1):
+            if i == 0 and j == 0:
+                current[i] = True
+            elif i == 0:
+                current[i] = previous[i] and s2[j - 1] == s3[j - 1]
+            elif j == 0:
+                current[i] = current[i - 1] and s1[i - 1] == s3[i - 1]
+            else:
+                current[i] = (previous[i] and s2[j - 1] == s3[i + j - 1]) or (current[i - 1] and s1[i - 1] == s3[i + j - 1])
+    return current[-1]
+
+
 if __name__ == '__main__':
-    interleaving = interleaving_naive
+    interleaving = interleaving_dynamic
+    print(interleaving(s1="ab", s2="bc", s3="bbac"))
+    print(interleaving(s1="", s2="", s3=""))
+    print(interleaving(s1="db", s2="b", s3="cbb"))
     print(interleaving(s1="aabcc", s2="dbbca", s3="aadbbcbcac"))
+    print(interleaving(s1="dbbca", s2="aabcc", s3="aadbbcbcac"))
     print(interleaving(s1="aabcc", s2="dbbca", s3="aadbbbaccc"))
-    # print(interleaving(*[
-    #     "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
-    #     "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
-    #     "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"]))
+    print(interleaving(*[
+        "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+        "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
+        "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"]))
