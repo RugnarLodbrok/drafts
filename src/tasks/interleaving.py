fa4adc3fc0eebb89afd1aa9c6e4b0c38b1441b43
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2020-02-08 20:15:24 +0300

    interleaving naive approach

--- src/tasks/interleaving.py
+++ src/tasks/interleaving.py
@@ -13,5 +13,41 @@ Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
 """
 
+
+def interleaving_naive(s1, s2, s3):
+    len1 = len(s1)
+    len2 = len(s2)
+    len3 = len(s3)
+
+    def recur(i, j, k):
+        while k < len3:
+            if i < len1 and j < len2 and s1[i] == s2[j]:
+                if s1[i] != s3[k]:
+                    return False
+                return recur(i + 1, j, k + 1) or recur(i, j + 1, k + 1)
+
+            elif i < len1 and s1[i] == s3[k]:
+                k += 1
+                i += 1
+                continue
+
+            elif j < len2 and s2[j] == s3[k]:
+                k += 1
+                j += 1
+                continue
+            else:
+                return False
+
+        return (i == len1) and (j == len2) and (k == len3)
+
+    return recur(0, 0, 0)
+
+
 if __name__ == '__main__':
-    pass
+    interleaving = interleaving_naive
+    print(interleaving(s1="aabcc", s2="dbbca", s3="aadbbcbcac"))
+    print(interleaving(s1="aabcc", s2="dbbca", s3="aadbbbaccc"))
+    # print(interleaving(*[
+    #     "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+    #     "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
+    #     "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"]))
