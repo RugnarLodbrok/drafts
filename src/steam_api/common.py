62b9391c659d06d502354fb6e9cfbe69a447c929
Georgy Usatenko <georgy.usatenko@picsart.com>
2024-04-27 16:13:08 +0300

    remove steam api - moved to own repo

--- src/steam_api/common.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-AnyDict = dict[str, Any]
-_AnyJsonItem = AnyDict | bool | None
-AnyJson = list[_AnyJsonItem] | _AnyJsonItem
--- src/steam_api/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Config(BaseSettings):
-    STEAM_API_KEY: str
-    STEAM_MY_ID: int
-
-
-config = Config()
